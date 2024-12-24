7b14ce4123a83873516846c303b75696be1ce525
k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>
2024-12-24 22:04:56 +0700

    anu
    Signed-off-by: k4ngcaribug <168145100+k4ngcaribug@users.noreply.github.com>

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/ysl/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/ysl/qassa*.zip rom:rom -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/ginkgo/XdCLO*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ginkgo/ && ls XdCLO*.zip)"
+      rclone copy ~/rom/out/target/product/ysl/XdCLO*.zip rom:rom -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ginkgo/ && ls XdCLO*.zip) Uploaded Successfully!"
 fi
